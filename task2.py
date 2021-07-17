1bb284d17ab39635c6b0ef17f4885014e97319a3
Pica490 <m_kuznecova@rambler.ru>
2021-07-18 00:07:43 +0500

    Correct file task2.py

--- task2.py
+++ task2.py
@@ -1,16 +1,18 @@
 import hashlib
 import json
 
-with open('result.json', encoding='utf-8') as f:
-    data = json.loads(f.read())
+def gen_hash(path):
 
-def gen_hash(start,end):
+    with open(f'result.json', encoding='utf-8') as f:
+        data = json.loads(f.read())
+        for string in data:
+            yield hash(string)
 
-    while start < end:
-        yield start
-        start += 1
+path = f'result.json'
+h = gen_hash(path)
 
-for item in gen_hash(0, len(data)-1):
-    print(hashlib.md5(data[item].encode('utf-8')).hexdigest())
+for item in h:
+    print(item)
+
 
 
